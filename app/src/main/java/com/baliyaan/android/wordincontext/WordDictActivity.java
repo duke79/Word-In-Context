06b63d03d5c70277828f4efc7ed5d2340529d8c2
Pulkit Singh <pulkitsingh01@gmail.com>
2018-01-10 19:51:23 +0530

    CoordinatorLayout | .

---
+++ app/src/main/java/com/baliyaan/android/wordincontext/WordDictActivity.java
@@ -0,0 +1,17 @@
+package com.baliyaan.android.wordincontext;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pulkit Singh on 1/10/2018.
+ */
+
+public class WordDictActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_dict);
+    }
+}
