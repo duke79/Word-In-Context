5168264fb218b0ea481d0c848b6cdcad30ff3ed2
Pulkit Singh <pulkitsingh01@gmail.com>
2018-01-13 13:15:11 +0530

    CoordinatorLayout | Examples view - test

--- app/src/main/java/com/baliyaan/android/wordincontext/WordDictActivity.java
+++ app/src/main/java/com/baliyaan/android/wordincontext/WordDictActivity.java
@@ -1,19 +1,45 @@
 package com.baliyaan.android.wordincontext;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.baliyaan.android.wordincontext.Components.Examples.MVP.ViewPort;
 
 /**
  * Created by Pulkit Singh on 1/10/2018.
  */
 
-public class WordDictActivity extends AppCompatActivity {
+public class WordDictActivity
+        extends AppCompatActivity
+        implements com.baliyaan.android.wordincontext.Components.Examples.MVP.Contract.Navigator{
+
+    private com.baliyaan.android.wordincontext.Components.Examples.MVP.Contract.Port _examplesPort = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         /*Set content view*/
         setContentView(R.layout.activity_word_dict);
+
+        _examplesPort = new ViewPort(this);
+        _examplesPort.onQueryTextSubmit("frugal");
+    }
+
+    @Override
+    public String getQuery() {
+        return "frugal";
+    }
+
+    @Override
+    public void onTryAgain() {
+        _examplesPort.onQueryTextSubmit("frugal");
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
     }
 }
