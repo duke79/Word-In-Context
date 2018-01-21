50cb02d89db56b94f886f6d3586e1087e916a84a
Pulkit Singh <pulkitsingh01@gmail.com>
2018-01-21 13:23:57 +0530

    LeakCanary(not tested); Firebase| Analytics (Search), CrashAnalytics, Message (not tested)

--- app/src/main/java/com/baliyaan/android/wordincontext/MyApplication.java
+++ app/src/main/java/com/baliyaan/android/wordincontext/MyApplication.java
@@ -3,6 +3,7 @@ package com.baliyaan.android.wordincontext;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by Pulkit Singh on 1/5/2018.
@@ -12,6 +13,16 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        /*LeakCanary*/
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
+        /*Stetho*/
         Stetho.initializeWithDefaults(this);
     }
 }
--- app/src/main/java/com/baliyaan/android/wordincontext/WordDictActivity.java
+++ app/src/main/java/com/baliyaan/android/wordincontext/WordDictActivity.java
@@ -8,11 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.baliyaan.android.mvp.Adapters.MVPNavigatorAdapter;
-import com.baliyaan.android.wordincontext.Components.Definition.Data.OfflineDictionary;
 import com.baliyaan.android.wordincontext.Components.Definition.MVP.Contract;
 import com.baliyaan.android.wordincontext.Components.Examples.MVP.ViewPort;
-
-import java.util.ArrayList;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 /**
  * Created by Pulkit Singh on 1/10/2018.
@@ -22,24 +20,21 @@ public class WordDictActivity
         extends AppCompatActivity {
 
     private Navigator _navigator;
+    private FirebaseAnalytics _firebaseAnalytics;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /* Firebase */
+        // Obtain the FirebaseAnalytics instance.
+        _firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
         /*Set content view*/
         setContentView(R.layout.activity_word_dict);
 
+        /*Initialize MVP Navigator*/
         _navigator = new Navigator(this);
-
-        //Todo: Dictionary is to be used from Definition.MVP.Presenter
-        OfflineDictionary dict = OfflineDictionary.GetInstance(this);
-        if(null != dict)
-        {
-            ArrayList<String> definitions = dict.GetWordsStartingWith("frugal",15);
-            if(definitions.size()>0)
-                ;
-        }
 
         /*
         * Search by intent (if any)
@@ -51,9 +46,14 @@ public class WordDictActivity
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        /*Handle search intent - exmaples & definition views*/
         String query = intent.getStringExtra("query");
-        //Todo: Replace with #onSearchIntent once SearchBox is also added to layout
         _navigator.onSearchIntent(query);
+        /*Log query in firebase*/
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.SEARCH_TERM, query);
+        if (null != _firebaseAnalytics)
+            _firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SEARCH, bundle);
     }
 
     @Override
@@ -97,7 +97,7 @@ public class WordDictActivity
         * Methods to be called from parent activity
          */
         void onSaveState(Bundle state) {
-            state.putString("query",_query);
+            state.putString("query", _query);
             _examplesPort.onSaveState(state);
             _definitionPort.onSaveState(state);
         }
@@ -111,6 +111,7 @@ public class WordDictActivity
         /**
          * Simulates search operation.
          * To be called from parent activity, specifically, to handle search intent from other activities.
+         *
          * @param query word string to search
          */
         void onSearchIntent(String query) {
@@ -119,12 +120,12 @@ public class WordDictActivity
             _definitionPort.onQueryTextSubmit(query);
 
             /*Update toolbar*/
-            String firstChar = query.substring(0,1);
+            String firstChar = query.substring(0, 1);
             firstChar = firstChar.toUpperCase();
-            query = firstChar + query.substring(1,query.length()).toLowerCase();
+            query = firstChar + query.substring(1, query.length()).toLowerCase();
 
             Toolbar toolbar = (Toolbar) findViewById(R.id.word_dict_toolbar);
-            if(null != toolbar)
+            if (null != toolbar)
                 toolbar.setTitle(query);
         }
 
